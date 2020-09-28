d91f1fccb2bad2be30f793b067fa305c1b461d6b
Mukul Chikhalkar <mukulchikhalkar.mc@gmail.com>
2020-09-28 17:48:54 +0530

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -1,4 +1,14 @@
-a = 10
-b = 10.0
+#A palindromic number reads the same both ways. The largest palindrome made from the product
+# of two 2-digit numbers is 9009 = 91 × 99.
+#Find the largest palindrome made from the product of two 3-digit numbers.
 
-print("%.2f" % (a + b), type(a + b))
+
+num = 0
+for a in range(999, 100, -1):
+    for b in range(a, 100, -1):
+        x = a * b
+        if x > num:
+            s = str(a * b)
+            if s == s[::-1]:
+                num = a * b
+print(num)
